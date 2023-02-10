79634c0ce7ef41ce534be604eae6e5afadb51251
Carlos <carlos.barahona.t@usach.cl>
2023-02-10 12:30:01 -0300

    Remade values refresh logic/abstraction, fixed IGT
    The refresh is now part of the state instead of the watchers, and made the memory values part of the state struct instead of a random struct that's created every update, that way I can keep using the old values if the memory read isn't successful instead of just crashing.
    
    IGT was  dumb, I was missing a parenthesis... I also have some ideas to make it robust in a TODO.

--- src/lib.rs
+++ src/lib.rs
@@ -6,17 +6,17 @@ const SUBMENUS_OPEN_PATH: [u64; 4] = [0x014B_FAB0, 0x0, 0x78, 0x28];
 const CURR_SECTION_FRAMES: [u64; 4] = [0x014BFE38, 0x10, 0xA8, 0x38];
 const TOTAL_FRAME_COUNT: [u64; 5] = [0x014BFE38, 0x0, 0x78, 0x10, 0x2C];
 
-fn read_value<T: Copy + std::fmt::Display + bytemuck::Pod>(process: &Process, main_module_addr: asr::Address, pointer_path: &[u64], watcher: &mut Watcher<T>, var_key: &str) -> Pair<T> {
+fn read_value<T: Copy + std::fmt::Display + bytemuck::Pod>(process: &Process, main_module_addr: asr::Address, pointer_path: &[u64], watcher: &mut Watcher<T>, var_key: &str) -> Result<Pair<T>, String> {
 
     match process.read_pointer_path64::<T>(main_module_addr.0, pointer_path) {
         Ok(mem_value) => {
             asr::timer::set_variable(var_key, &format!("{}", mem_value));
             //asr::print_message(&format!("{}", mem_value));
-            watcher.update(Some(mem_value)).copied().unwrap()
+            Ok(*watcher.update_infallible(mem_value))
         },
         Err(e) => {
-            asr::print_message(&format!("Could not refresh Submenus open value: {:?}", e));
-            watcher.update(None).copied().unwrap()
+            asr::print_message(&format!("Could not refresh '{}' value: {:?}", var_key, e));
+            Err(format!("Could not refresh '{}' value: {:?}", var_key, e))
         }
     }
 
@@ -38,6 +38,7 @@ impl ProcessInfo {
 struct State {
     process_info: Option<ProcessInfo>,
     watchers: Watchers,
+    values: MemoryValues,
     started_up: bool,
 }
 
@@ -53,29 +54,6 @@ struct MemoryValues {
     acumm_frames: Pair<i32>,
 }
 
-impl Watchers {
-
-    fn refresh(&mut self, process: &Process) -> Result<MemoryValues, &str> {
-
-        let Ok(main_module_addr) = process.get_module_address("SOR4.exe")
-        else {
-            asr::print_message("COULD NOT GET MODULE ADDRESS");
-            return Err("COULD NOT GET MODULE ADDRESS");
-        };
-
-        // refresh watchers
-        let return_vals = MemoryValues {
-            submenus_open: read_value::<i32>(process, main_module_addr, &SUBMENUS_OPEN_PATH, &mut self.submenus_open, "Submenus"),                
-            current_section_frames: read_value::<i32>(process, main_module_addr, &CURR_SECTION_FRAMES, &mut self.current_section_frames, "Current section frames"),
-            acumm_frames: read_value::<i32>(process, main_module_addr, &TOTAL_FRAME_COUNT, &mut self.acumm_frames, "Accumulated Frames"),
-        };
-
-        Ok(return_vals)
-
-    }
-
-}
-
 impl State {
 
     fn startup(&mut self) {
@@ -87,7 +65,36 @@ impl State {
         asr::print_message("ATTACHED!!!!!!!!!!");
         asr::set_tick_rate(60.0);
         asr::timer::set_variable("Submenus", "-");
-        asr::timer::set_variable("IGT", "-");
+        asr::timer::set_variable("Current section frames", "-");
+        asr::timer::set_variable("Accumulated Frames", "-");
+    }
+
+    fn refresh_mem_values(&mut self) -> Result<&str, &str> {
+
+        // todo: make this part of precess info struct and only do once
+        let process = &self.process_info.as_ref().unwrap().game;
+
+        let Ok(main_module_addr) = process.get_module_address("SOR4.exe")
+        else {
+            asr::print_message("COULD NOT GET MODULE ADDRESS");
+            return Err("COULD NOT GET MODULE ADDRESS");
+        };
+
+        // refresh values with watcher updates
+        match read_value::<i32>(process, main_module_addr, &SUBMENUS_OPEN_PATH, &mut self.watchers.submenus_open, "Submenus") {
+            Ok(value) => self.values.submenus_open = value,
+            Err(_) => {}
+        }; 
+        match read_value::<i32>(process, main_module_addr, &CURR_SECTION_FRAMES, &mut self.watchers.current_section_frames, "Current section frames") {
+            Ok(value) => self.values.current_section_frames = value,
+            Err(_) => {}
+        };
+        match read_value::<i32>(process, main_module_addr, &TOTAL_FRAME_COUNT, &mut self.watchers.acumm_frames, "Accumulated Frames") {
+            Ok(value) => self.values.acumm_frames = value,
+            Err(_) => {}
+        }
+
+        Ok("Success")
     }
 
     fn update(&mut self) {
@@ -105,18 +112,25 @@ impl State {
             return;
         }
 
-        let process = &self.process_info.as_ref().unwrap().game;
-        // directly access the mem values using a struct made of Pairs, watchers only return a mem value after using update() so this abstracts that
-        let Ok(mem_values) = self.watchers.refresh(process) else { return };
+        // refresh mem values if possible, the values will be updated using the watchers
+        if self.refresh_mem_values().is_err() {
+            return;
+        }
 
         // reset condition
-        if mem_values.submenus_open.current == 0 && mem_values.submenus_open.old == 2 {
+        if self.values.submenus_open.current == 0 && self.values.submenus_open.old == 2 {
             asr::timer::reset();
         }
 
         // igt
-        let game_time = asr::time::Duration::seconds((mem_values.current_section_frames.current + mem_values.acumm_frames.current / 60).try_into().unwrap()  );
-        asr::timer::set_game_time(game_time);
+        // todo: make igt into a atruct with values to add in a Pair and implement methods to only update it if the new calculated value is appropriate
+        //       and make it part of state
+        
+        if self.values.current_section_frames.current - 1000 < self.values.current_section_frames.old {
+            let igt_value = (self.values.current_section_frames.current + self.values.acumm_frames.current) as i64 * 1000 / 60;
+            let game_time = asr::time::Duration::milliseconds(igt_value);
+            asr::timer::set_game_time(game_time);
+        }
 
 
     }
@@ -128,6 +142,11 @@ static LS_CONTROLLER: Spinlock<State> = const_spinlock(State {
         submenus_open: Watcher::new(),
         current_section_frames: Watcher::new(),
         acumm_frames: Watcher::new(),
+    },
+    values: MemoryValues { 
+        submenus_open: Pair { old: 0, current: 0 }, 
+        current_section_frames: Pair { old: 0, current: 0 },
+        acumm_frames: Pair { old: 0, current: 0 }
     },
     started_up: false,
 });
